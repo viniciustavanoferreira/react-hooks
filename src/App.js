5a4f9d69f8cead204cb20660e2f3f3c255383e22
Vinicius T. Ferreira <vinicius.tavano@riachuelo.com.br>
2022-09-19 15:30:41 -0300

    feat: add project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [repository, setRepository] = useState([]);
+
+    useEffect(() => {
+        async function callApi() {
+            const response = await fetch("https://api.github.com/users/viniciustavanoferreira/repos");
+            const repositories = await response.json();
+            setRepository(repositories);
+        }
+        callApi();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo}
+                    className="App-logo"
+                    alt="logo"/>
+
+                <a className="App-link" href="https://viniciustavanoferreira.wordpress.com/" target="_blank" rel="noopener noreferrer">
+                    Vinicius T. Fereira - Systems Analyst
+                </a>
+
+                <p>
+                    List of some of my GitHub repositories:
+                </p>
+
+                <ul>
+                  {repository.map(repository => (<li key={repository.id}>{repository.name}</li>))}
+                </ul>
+            </header>
+
+        </div>
+
+    );
 }
 
 export default App;
